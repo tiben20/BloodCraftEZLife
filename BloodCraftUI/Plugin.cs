4d3ecf5de1b43980dcc813d92b626b8c40b1fd04
Alexander Smirnov <panthernet@mail.ru>
2025-04-13 22:06:32 +0200

    Version 1.0.3 bump

--- BloodCraftUI/Plugin.cs
+++ BloodCraftUI/Plugin.cs
@@ -132,6 +132,6 @@ namespace BloodCraftUI
     {
         public const string PLUGIN_GUID = "panthernet.BloodCraftUI";
         public const string PLUGIN_NAME = "BloodCraftUI";
-        public const string PLUGIN_VERSION = "1.0.1";
+        public const string PLUGIN_VERSION = "1.0.3";
     }
 }
