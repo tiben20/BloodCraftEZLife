ee726d370dc72fb19484326afe26f6068a8d3bc4
Benoit Plourde <curtis_20_b@hotmail.com>
2025-08-16 21:55:28 -0400

    Forgot one file from the multi cell panel, initial commit of addition settings button in the main menu

--- BloodCraftUI/Patches/PlayerlistPatch.cs
+++ BloodCraftUI/Patches/PlayerlistPatch.cs
@@ -12,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using Unity.Entities.CodeGeneratedJobForEach;
 using BloodCraftEZLife.UI.ModContent;
+using UnityEngine;
+using TMPro;
 
 namespace BloodCraftEZLife.Patches;
 
@@ -58,6 +60,80 @@ internal static class PlayerListPatch
         LogUtils.LogInfo($"OnStopRunning");
 
       
+    }
+    [HarmonyPatch(typeof(FullscreenMenu_OuterTabEntry), nameof(FullscreenMenu_OuterTabEntry.Refresh))]
+    [HarmonyPostfix]
+    private static void OnStartRunning_53CF59E2_b__1(FullscreenMenu_OuterTabEntry __instance,bool isSelected, bool requirementMet, bool highlight)
+    {
+        if (__instance.name == "TabEntry_System")
+        {
+            LogUtils.LogInfo($"___OnStartRunning_53CF59E2_b__38_0");
+        }
+        
+
+
+    }
+
+    [HarmonyPatch(typeof(OptionsMenu), nameof(OptionsMenu.Update))]
+    [HarmonyPostfix]
+    private static void OptionsMenuUpdate(OptionsMenu __instance)
+    {
+        
+        //SoundTabButton
+        //SimpleStunButton
+        //Verify we didnt add it already
+        /* HEADER BUTTON IN OPTIONS MENU EZLife */
+        SimpleStunButton simpleButton = __instance.SoundTabButton;
+        //Check if button exist if it exist quit
+        Transform btnSearch = simpleButton.transform.parent.Find("EZLifeButton");
+        if (btnSearch != null)
+            return;
+
+        var settingsButton = UnityEngine.Object.Instantiate(simpleButton.gameObject,simpleButton.transform.parent);
+        settingsButton.name = "EZLifeButton";
+
+        var newSettingsButton = settingsButton.GetComponent<SimpleStunButton>();
+
+        var label = settingsButton.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        if (label != null)
+            label.text = "EZLife";
+        //TODO
+        newSettingsButton.m_OnClick = null;
+
+
+        TextMeshProUGUI[] texts = __instance.GetComponentsInChildren<TextMeshProUGUI>(true);
+        // (true) means include inactive objects
+
+        foreach (TextMeshProUGUI tmp in texts)
+        {
+            if (tmp.text == "Language")
+            {
+                Debug.Log("Found Language");
+            }
+            if (tmp.text == "General")
+            {
+                Debug.Log("General");
+            }
+            if (tmp.text == "Text Scaling")
+            {
+                Debug.Log("Text Scaling");
+            }
+            Debug.Log("Found TMP: " + tmp.text);
+        }
+        foreach (var cmp in __instance.SoundPanel.GetComponents<Component>())
+        {
+
+            LogUtils.LogInfo(cmp.transform.ToString());
+            foreach (var childCmp in cmp.GetComponentsInChildren<Component>())
+            {
+                ;
+                LogUtils.LogInfo("Child :" + childCmp.transform.ToString());
+            }
+
+        }
+
+
+
     }
 
 }
---
+++ BloodCraftUI/UI/CustomLib/Cells/Handlers/ConfigCellHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using BloodCraftEZLife.UI.UniverseLib.UI.Widgets.ScrollView;
+using BloodCraftEZLife.Utils;
+using BloodmoonUI.UI.CustomLib.Cells;
+using Unity.Collections;
+
+namespace BloodCraftEZLife.UI.CustomLib.Cells.Handlers
+{
+    /// <summary>
+    /// A helper to create and handle a simple <see cref="ScrollPool{T}"/> of Buttons, which can be backed by any data.
+    /// </summary>
+    public class ConfigCellHandler<TData, TCell> : ICellPoolDataSource<TCell> where TCell : CellBase, IConfigurableCell<TData>
+    {
+        public ScrollPool<TCell> ScrollPool { get; private set; }
+
+        public int ItemCount => CurrentEntries.Count;
+        public List<TData> CurrentEntries { get; } = new();
+
+        protected readonly Func<List<TData>> GetEntries;
+        protected readonly Action<TCell, int> SetICell;
+        protected readonly Func<TData, string, bool> ShouldDisplay;
+        protected readonly Action<TData,int> OnCellChanged;
+        protected readonly Action<TCell, int,float> OnSetOpacity;
+
+        public string CurrentFilter
+        {
+            get => _currentFilter;
+            set => _currentFilter = value ?? "";
+        }
+        private string _currentFilter;
+
+        /// <summary>
+        /// Create a wrapper to handle your Button ScrollPool.
+        /// </summary>
+        /// <param name="scrollPool">The ScrollPool&lt;ButtonCell&gt; you have already created.</param>
+        /// <param name="getEntriesMethod">A method which should return your current data values.</param>
+        /// <param name="setICellMethod">A method which should set the data at the int index to the cell.</param>
+        /// <param name="shouldDisplayMethod">A method which should determine if the data at the index should be displayed, with an optional string filter from CurrentFilter.</param>
+        /// <param name="onCellClickedMethod">A method invoked when a cell is clicked, containing the data index assigned to the cell.</param>
+        public ConfigCellHandler(ScrollPool<TCell> scrollPool, Func<List<TData>> getEntriesMethod,
+            Action<TCell, int> setICellMethod, Func<TData, string, bool> shouldDisplayMethod,
+            Action<TData,int> onCellChangedMethod)
+        {
+            ScrollPool = scrollPool;
+
+            GetEntries = getEntriesMethod;
+            SetICell = setICellMethod;
+            ShouldDisplay = shouldDisplayMethod;
+            OnCellChanged = onCellChangedMethod;
+        }
+
+        public void RefreshData()
+        {
+            var allEntries = GetEntries();
+            CurrentEntries.Clear();
+
+            foreach (var entry in allEntries)
+            {
+                if (!string.IsNullOrEmpty(_currentFilter))
+                {
+                    if (!ShouldDisplay(entry, _currentFilter))
+                        continue;
+                }
+
+                CurrentEntries.Add(entry);
+            }
+        }
+
+        public virtual void OnCellBorrowed(TCell cell)
+        {
+            cell.OnValueChanged += OnCellChanged;
+        }
+
+        public virtual void SetCell(TCell cell, int index)
+        {
+            if (CurrentEntries == null)
+                RefreshData();
+
+            if (index < 0 || index >= CurrentEntries.Count)
+                cell.Disable();
+            else
+            {
+                cell.Enable();
+
+                cell.SetIndex(index);
+                SetICell(cell, index);
+            }
+        }
+    }
+}
